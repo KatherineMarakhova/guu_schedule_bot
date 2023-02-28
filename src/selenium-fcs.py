12d0c33d8a51695623eee1687462b9c0655247d6
Murka <katherine.marakhova@gmail.com>
2023-02-28 12:57:38 +0300

    Добавила функцию, которая разделяет институты, если их два на одном листе.

--- src/selenium-fcs.py
+++ src/selenium-fcs.py
@@ -2,17 +2,18 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 
-PATH = "/Users/katherine.marakhova/PycharmProjects/exampleBot/files"
-# Google Chrome
-options = webdriver.ChromeOptions()
+def get_file():
+    PATH = "/Users/katherine.marakhova/PycharmProjects/exampleBot/files"
+    # Google Chrome
+    options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": PATH}
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=options)
 
-prefs = {"download.default_directory": PATH}
-options.add_experimental_option("prefs", prefs)
-
-driver = webdriver.Chrome(options=options)
-driver.get('https://guu.ru/студентам/расписание-сессий/schedule/')
-time.sleep(3)
-driver.get("https://guu.ru/wp-content/uploads/4-курс-бакалавриат-ОФО-42.xlsx")
-#driver.get('https://guu.ru/wp-content/uploads/3-курс-бакалавриат-ОФО-50.xlsx')
-time.sleep(3)
-driver.quit()
+    driver.get('https://guu.ru/студентам/расписание-сессий/schedule/')
+    time.sleep(3)
+    # селекторы надо обязательно переписать!!!!!!!!!!!
+    driver.get("https://guu.ru/wp-content/uploads/4-курс-бакалавриат-ОФО-42.xlsx")
+    #driver.get('https://guu.ru/wp-content/uploads/3-курс-бакалавриат-ОФО-50.xlsx') #файл третьего курса
+    time.sleep(3)
+    driver.quit()
